a2b53ae4e4f5a614553519a7ae3953dbedd688d4
Rodrigo Lezama <cordolezama@gmail.com>
2024-05-07 10:22:10 -0300

    config tailwind styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				noto: ['Noto Serif Display', 'serif'],
+				cloud: ['IBM Plex Mono', 'monospace'],
+				glacial: ['Iosevka Aile', 'sans-serif'],
+			},
+			colors: {
+				bgPrimary:'#F4ECE7',
+				wine:'#721F49',
+				brown:'#5B4437',
+				whiteVariation: '#FFFBF8'
+			}
+		},
 	},
 	plugins: [],
 }
